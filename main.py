7f0b3d48d4416b1bbef2ce77e38183d801404ec9
Robin <r.bechsteinqprotonmail.com>
2021-03-23 21:21:20 +0100

    !wpx v1

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from discord.utils import get
-import discord  # pls wörk
+import discord
 import os
 import datetime
 from datetime import timedelta
@@ -9,9 +8,10 @@ from keepAlive import keep_alive
 client = discord.Client()
 
 customEmoji1 = "eis"
-positive = "👍" 
+positive = "👍"
 negative = "👎"
 maybe = "🤷"
+
 raidCombList = ["Dienstag;20:00;2",
                 "Mittwoch;20:00;3",
                 "Donnerstag;20:00;4",
@@ -20,8 +20,29 @@ raidCombList = ["Dienstag;20:00;2",
                 "Samstag;20:00;6",
                 "Sonntag;15:00;0",
                 "Sonntag;20:00;0",
-                "Montag;20:00;1"]
-                # liste von Raidterminen: [Wochentag;Uhrzeit;Wochentag als Zahl(Sonntag = 0, Samstag = 6)]
+                "Montag;20:00;1"
+                ]  # liste von Raidterminen: [Wochentag;Uhrzeit;Wochentag als Zahl(Sonntag = 0, Samstag = 6)]
+
+pEmoji1 = "🟦"
+pEmoji2 = "🟩"
+pEmoji3 = "🟨"
+pEmoji4 = "🟥"
+pEmojiX = "✅"
+
+pExplain = "**Erklärung für Prioritäts-Abstimmung:**\n\
+            `" + pEmoji1 + "` Ich möchte, weil ich noch was brauch\n\
+            `" + pEmoji2 + "` Ich möchte, weil ich bock hab, lasse aber anderen, die noch was brauchen, den Vortritt\n\
+            `" + pEmoji3 + "` Ich mach mit wenn jemand fehlt\n\
+            `" + pEmoji4 + "` Ich möchte auf gar keinen fall\n\
+            `" + pEmojiX + "` mir is alles egal ich will einfach irgendwas raiden"
+
+pRaidList = ["Deep Stone Crypt"
+             "Garden of Salvation"
+             "Last Wish"
+             "Last Wish - only Riven(3x)"
+             "Ich möchte einfach raiden - egal was"
+             ]
+pX = "mir is alles egal ich will einfach irgendwas raiden"
 
 wishEffects = [
     "grants an Ethereal Key.",
@@ -40,14 +61,18 @@ wishEffects = [
     "spawns Corruptes eggs."
 
 ]
+helopText = "Available Commands: \n\
+      `!throne`: Shattered Throne 1st Encounter map \n\
+      `!heresy`: Pit of Heresy 4th Encounter map \n\
+      `!dsc 1`: DSC 1st Encounter map \n\
+      `!dsc 3`: DSC 3rd Encounter map\n\
+      `!wish all`: Overview over all Wishes \n\
+      `!wish` + number(1-14): shows the selected wish\n\
+      `!wahrheit`: Beschert dir die Erleuchtung nach der du dein Leben lang gesucht hast! "
 
-
-def log(text):
+"""def log(text):
     with open("log.csv", "a") as file:
-        file.write(text + "\n")
-
-
-
+        file.write(text + "\n")"""
 
 
 @client.event
@@ -61,41 +86,81 @@ async def on_ready():
     print('{0.user} is online at '.format(client) + now)
 
 
-
-
-
 @client.event
 async def on_message(message):
+    # function Setup
     def loggeneral():
         with open("log.csv", "a") as file:
             file.write(message.author.name + ";" + now + ";" + message.content + "\n")
-    if False:
-        await message.channel.send("Robin hat Recht!")
 
+    async def wp():
+        await message.delete()
+
+        addent = int((datetime.datetime.now()).strftime("%w"))
+        addent += -2
+        if addent >= 3:
+            addent += -7
+
+        date = datetime.datetime.now() - timedelta(days=addent)
+
+        for raidComb in raidCombList:
+            raidComb = raidComb.split(";")
+            raidDay = raidComb[0]
+            raidTime = raidComb[1]
+
+            if date.strftime("%w") != raidComb[2]:
+                # verhindert das bei wiederholung von wochentagen das datum erhöht wird
+                date = date + timedelta(days=1)
+
+            rDay = date.strftime("%d")
+            month = date.strftime("%m")
+            # year = date.strftime("%Y")
+            dateToPrint = rDay + "." + month + "."  # + year
+            await message.channel.send(raidDay + " " + dateToPrint + " " + raidTime)
+
+    async def wpraids():
+        await message.delete()
+        await message.channel.send(pExplain)
+        for raid in pRaidList:
+            await message.channel.send(raid)
+
+
+    # time Setup
     nowDay = (datetime.datetime.now()).strftime("%d")
     nowMonth = (datetime.datetime.now()).strftime("%m")
     nowYear = (datetime.datetime.now()).strftime("%Y")
     nowTime = (datetime.datetime.now()).strftime("%X")
     now = nowDay + "." + nowMonth + "." + nowYear + " " + nowTime
 
+    # Reactions
+    # Reactions RaidDates
     if message.author == client.user:
         for day in raidCombList:
 
             if (message.content.split(" "))[0] == day.split(";")[0]:
-                await message.add_reaction(discord.utils.get(client.emojis, name=customEmoji1))
+                # await message.add_reaction(discord.utils.get(client.emojis, name=customEmoji1))
                 await message.add_reaction(positive)
                 await message.add_reaction(negative)
                 await message.add_reaction(maybe)
 
-    
-    
-    
-    
-    
-    
+    # Reactions RaidPriority
+    if message.author == client.user:
+        for raidName in pRaidList:
+
+            if message.content == raidName:
+                await message.add_reaction(pEmoji1)
+                await message.add_reaction(pEmoji2)
+                await message.add_reaction(pEmoji3)
+                await message.add_reaction(pEmoji4)
+
+        if message.content == pX:
+            await message.add_reaction(pEmojiX)
+
+    # ignoring bots
     if message.author == client.user:
         return
 
+    # commands
     if "!throne" in message.content:
         loggeneral()
         await message.delete()
@@ -113,12 +178,10 @@ async def on_message(message):
         await message.delete()
         number = message.content.split("!wish ")[1]
         if number == "all":
-            i = 0;
+            i = 0
             for effect in wishEffects:
                 i = i + 1
                 await message.channel.send(str(i) + ". Wish: " + effect)
-
-
         else:
             await message.channel.send(number + ". Wish: " + wishEffects[int(number) - 1])
             await message.channel.send(file=discord.File("guides/wishes/wish-" + number + ".jpg"))
@@ -134,15 +197,7 @@ async def on_message(message):
     if "!helöp" in message.content:
         loggeneral()
         await message.delete()
-        await message.channel.send("Available Commands: \n\
-      `!throne`: Shattered Throne 1st Encounter map \n\
-      `!heresy`: Pit of Heresy 4th Encounter map \n\
-      `!dsc 1`: DSC 1st Encounter map \n\
-      `!dsc 3`: DSC 3rd Encounter map\n\
-      `!wish all`: Overview over all Wishes \n\
-      `!wish` + number(1-14): shows the selected wish\n\
-      `!wahrheit`: Beschert dir die Erleuchtung nach der du dein Leben lang gesucht hast! "
-                                   )
+        await message.channel.send(helopText)
 
     if "!wahrheit" in message.content:
         loggeneral()
@@ -150,7 +205,6 @@ async def on_message(message):
 
     if "!xxtime" in message.content:
         loggeneral()
-        #log(message.author.name + " used !xxtime at " + now)
         await message.delete()
         xxtime = (datetime.datetime.now()).strftime("%c")
         for i in range(1, 60):
@@ -160,60 +214,31 @@ async def on_message(message):
             else:
                 sleep(0.49)
 
-
-    async def wp():
-        await message.delete()
+    if "!wp" in message.content:
         loggeneral()
-        #log(message.author.name + " used !wp at " + now)
+        await wp()
 
-        addent = int((datetime.datetime.now()).strftime("%w"))
-        addent += -2
-        if addent >= 3:
-            addent += -7
-
-        date = datetime.datetime.now() - timedelta(days=addent)
-
-        for raidComb in raidCombList:
-            raidComb = raidComb.split(";")
-            raidDay = raidComb[0]
-            raidTime = raidComb[1]
-
-            if date.strftime("%w") != raidComb[2]:  # verhindert das bei wiederholung von wochentagen das datum erhöht wird
-                date = date + timedelta(days=1)
-
-            day = date.strftime("%d")
-            month = date.strftime("%m")
-            # year = date.strftime("%Y")
-            dateToPrint = day + "." + month + "."  # + year
-            await message.channel.send(raidDay + " " + dateToPrint + " " + raidTime)
-
-      
-
-    if "!weeklypoll" in message.content or "!wp" in message.content:
-      loggeneral()
-      await wp()
-
-     
+    if "!wpx" in message.content:
+        loggeneral()
+        await wp()
+        await wpraids()
 
     if "!send" in message.content:
         text = message.content
         text = text.replace("!send", "")
 
         loggeneral()
-        #log(message.author.name + " used !send at " + now + " with " + text)
         await message.channel.send(text)
         await message.delete()
 
     if "!nein" in message.content:
         await message.delete()
         loggeneral()
-        #log(message.author.name + " used !nein at " + now)
         for i in range(5):
             await message.channel.send("**NEIN**")
 
     if "!clear" in message.content:
         loggeneral()
-        #log(message.author.name + " used !clear at " + now)
         args = message.content.split(" ")
         if len(args) == 2 and args[1].isdigit():
             limit = int(args[1]) + 1
