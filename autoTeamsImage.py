ad19d67b358828b4d247e98e512ba20b28bdf158
BechsteinR <71779437+BechsteinR@users.noreply.github.com>
2021-08-20 18:48:38 +0000

    stuff

--- autoTeamsImage.py
+++ autoTeamsImage.py
@@ -9,7 +9,6 @@ async def autoTeamsImage(histchannel, teamchannel, client):
     testch=client.get_channel(727474264315396117)
     testch = teamchannel
     dayListsDic = {}
-    pListsDic = {}
     playerList = []
 
     async for message in histchannel.history(oldest_first=True):
@@ -115,7 +114,14 @@ async def autoTeamsImage(histchannel, teamchannel, client):
     
     
 
-'''
+async def autoTeamsImage2(histchannel, teamchannel, client):
+    histchannel = client.get_channel(790270264385863681)
+    testch=client.get_channel(727474264315396117)
+    testch = teamchannel
+    pListsDic = {}
+    playerList = []
+
+    async for message in histchannel.history(oldest_first=True):
         for raid in pRaidList:
             if message.content == raid:
                 plist1 = []
@@ -128,7 +134,11 @@ async def autoTeamsImage(histchannel, teamchannel, client):
                     reacList = []
                     async for user in reaction.users():
                         if user != client.user:
-                            reacList.append(user.mention)
+                          reacList.append(user.avatar_url)
+                    
+                    if not user.avatar_url in playerList:
+                                playerList.append(user.avatar_url)
+                                
                     if str(reaction) == pEmoji1:
                         plist1 = reacList
                     elif str(reaction) == pEmoji2:
@@ -152,8 +162,80 @@ async def autoTeamsImage(histchannel, teamchannel, client):
                 reacList = []
                 async for user in reaction.users():
                     if user != client.user:
-                        reacList.append(user.mention)
+                        reacList.append(user.avatar_url
                 if str(reaction) == pEmojiX:
                     plistX = reacList
-'''
- 
+
+     print(pListsDic)
+    color_foreground = (230, 230, 230)
+    color_background = (35, 39, 42)
+    im_out = Image.new(mode = "RGB", size = ((len(pListsDic)*200)+200, (len(playerList)*200)+200), color = color_background)
+    draw = ImageDraw.Draw(im_out)
+
+    #table base
+    for i in len(pListsDic)+1:
+        for j in range(len(playerList)+1):
+            x = i * 200
+            y = j * 200
+            draw.rectangle([x,y,x+199,y+199], outline = color_foreground)
+
+    #hardlines
+    draw.rectangle([0,197,im_out.size[0],201],fill=color_foreground)#horizontal
+    draw.rectangle([197,0,201,im_out.size[1]],fill=color_foreground)#vertical
+
+    # writings
+    i = 1
+    font = ImageFont.truetype("guides/autoTeamsImage/arial.ttf", size=30)
+    for elem in pListsDic.keys():
+      txt = elem
+      draw.text(xy = [((i*200)+100),100], text = txt, anchor="mm", align = "center", font=font)
+      i += 1
+
+
+
+    # player avatars
+    i = 1
+    for player in playerList:
+      try:
+          #player_avatar = Image.open(urllib2.urlopen(player))
+          player_avatar = Image.open(requests.get(player, stream=True).raw)
+          print(player_avatar.size)
+          player_avatar = player_avatar.resize([150,150])
+          im_out.paste(player_avatar, box = [25,(25+(i*200))])
+      except Exception as e:
+        print(str(i) + " error " + str(e))
+      i += 1
+
+    #symbols
+    i = 1
+    for raid in pListsDic.values():
+        for entry in raid:
+            for player in entry:
+                #if entry == day[0]:
+                    #continue
+                j = playerList.index(player) + 1
+                x = i * 200 + 25 
+                y = j * 200 + 25
+                if player in raid[0]:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+                elif player in raid[1]:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+                    elif player in raid[2]:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+                    elif player in raid[3]:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+                    elif player in raid[4]:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+                    elif player in plistX:
+                    symbol = Image.open("guides/autoTeamsImage/symbols/raid_low/.png")
+
+                
+                symbol = symbol.resize([150,150])
+                im_out.paste(symbol, box = [x,y])
+            
+
+        i += 1
+
+
+    im_out.save("guides/autoTeamsImage/out2.jpg")
+    await testch.send(file=discord.File("guides/autoTeamsImage/out2.jpg"))
